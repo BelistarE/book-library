70cbd6a5f0f132bf4abc53821d47742ebcd825a4
Isabeli <86092004+BelistarE@users.noreply.github.com>
2024-07-07 14:45:59 -0700

    added background and basic html

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function Book(name, author, pages, read) {
-    this.name = name;
+
+const myLibrary = [];
+
+
+
+function Book(title, author, pages, read) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.info = function() {
+        let readStatus = this.read ? "read" : "not read yet";
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+    };
+  }
+
+  let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+
+  console.log(theHobbit.info()); 
+
+
+
+
+function addBookToLibrary() {
+    // do stuff here
   }
